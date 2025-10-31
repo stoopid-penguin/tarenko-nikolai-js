7daad2481f6c092416392b9a4bee608431f28dc5
stoopid-penguin <lostumbrella0108@yahoo.com>
2025-10-31 20:38:23 +0300

    del unused code

--- 9/main.js
+++ 9/main.js
@@ -1,3 +1,4 @@
+//1
 const userName = prompt('Ведите name');
 const userEmail = prompt('Ведите email');
 const userPhone = prompt('Ведите phone');
@@ -24,7 +25,7 @@ createUser({
 const postTitle = prompt('Ведите postTitle');
 const postBody = prompt('Ведите postBody');
 const postUserId = prompt('Ведите postUserId');
-
+//2
 async function createPost(post) {
     try {
         const res = await fetch (`https://jsonplaceholder.typicode.com/posts`, {
@@ -44,61 +45,3 @@ createPost({
   userId: postUserId
 }
 );
-
-/* const wrapper = document.querySelector('#outputContainer')
-async function getProducts() {
-    try {
-        const res = await fetch ('https://fakestoreapi.com/products')
-        const products = await res.json();
-        wrapper.innerHTML = JSON.stringify(products, 2, null);
-    } catch(e) {
-        console.error('Не удалось получить продукты: ' + e)
-    }
-}
-
-// getProducts();
-
-async function getProduct(id) {
-    try {
-        const res = await fetch (`https://fakestoreapi.com/products/${id}`)
-        const products = await res.json();
-        wrapper.innerHTML = JSON.stringify(products, 2, null);
-    } catch(e) {
-        console.error(`Не удалось получить продукт с id ${id}: ${e} `)
-    }
-}
-
-getProduct(10); */
-/* async function updateProduct(product, id) {
-    try {
-        const res = await fetch (`https://fakestoreapi.com/products/${id}`, {
-            method: "PUT"
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(product)
-
-        })
-        const updatedProduct = await res.json();
-        console.log('product created!')
-    } catch(e) {
-        console.error(`Не удалось обновить продукт ${e}`)
-    }
-}
-
-updateProduct({
-    title: 'NFT Носки',
-    price: 100
-}, 10);
-
-async function deleteProduct(id) {
-    try {
-        const res = await fetch (`https://fakestoreapi.com/products/${id}`, {
-            method: "DELETE"
-        })
-        const updatedProduct = await res.json();
-        console.log('product deleted!')
-    } catch(e) {
-        console.error(`Не удалось удалить продукт ${e}`)
-    }
-}
-
-deleteProduct(10); */
